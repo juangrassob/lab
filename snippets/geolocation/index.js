945924c6ae111def00c24a38dcb32df581bda546
Juan <juangrassob@gmail.com>
2025-01-27 12:35:15 -0300

    Geolocation

---
+++ snippets/geolocation/index.js
@@ -0,0 +1,18 @@
+
+const fs = require('fs');
+const Reader = require('@maxmind/geoip2-node').Reader;
+
+
+process.title = 'geoip_lookup';
+
+const dbBuffer = fs.readFileSync('./GeoLite2-Country.mmdb');
+
+const reader = Reader.openBuffer(dbBuffer);
+
+console.time('geoip-query');
+response = reader.country('2800:2285:9000:2de:5c41:b317:808a:38d7');
+console.timeEnd('geoip-query');
+
+console.log(response.country.isoCode);
+
+setInterval(() => { }, 1000);
