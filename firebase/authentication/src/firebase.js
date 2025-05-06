ae4a1b6bc7de2c9fa293ad83c66966af29b8d8b0
Juan <juangrassob@gmail.com>
2025-05-06 15:45:48 -0300

    Removed old api key

--- firebase/authentication/src/firebase.js
+++ firebase/authentication/src/firebase.js
@@ -8,16 +8,15 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAf4I30iAIZ_dFlTqgTJrMKch-fnEjs9ik",
-    authDomain: "trip-planner-264c6.firebaseapp.com",
-    projectId: "trip-planner-264c6",
-    storageBucket: "trip-planner-264c6.firebasestorage.app",
-    messagingSenderId: "426449055948",
-    appId: "1:426449055948:web:43e8115457375743159269",
-    measurementId: "G-DMW2RVZ08D"
+  apiKey: "-fnEjs9ik",
+  authDomain: "trip-planner-264c6.firebaseapp.com",
+  projectId: "trip-planner-264c6",
+  storageBucket: "trip-planner-264c6.firebasestorage.app",
+  messagingSenderId: "426449055948",
+  appId: "1:426449055948:web:43e8115457375743159269",
+  measurementId: "G-DMW2RVZ08D",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-
